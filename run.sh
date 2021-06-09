09d25671a70448d916062f4f37804bded216112f
Tiago Tavares <0xtiago>
2021-06-09 18:55:40 -0300

    Refactor again

--- run.sh
+++ run.sh
@@ -34,10 +34,6 @@ wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
 go version
 
-#Moving all above GO bins to /usr/bin
-mv /usr/local/go/bin/* /usr/bin
-mv ~/go/bin/* /usr/bin
-
 #Installing anew
 go get -u github.com/tomnomnom/anew
 
