5e74e20bd35001f64f321d5ef8728499d72b29ea
Tiago Tavares <0xtiago>
2021-06-09 18:51:31 -0300

    Refactor

--- run.sh
+++ run.sh
@@ -24,14 +24,14 @@ fi
 #=============================================================================================
 echo -e "${RED}[+] Installing all requirements${NC}"
 #Installing packages
-sudo apt-get update && sudo apt-get install net-tools htop vim gzip zip git python3-pip jq tmux snap -y
+apt-get update && apt-get install net-tools htop vim gzip zip git python3-pip jq tmux snap -y
 
 #Installing newer GO
-sudo apt purge golang
-sudo apt autoremove golang 
+apt purge golang
+apt autoremove golang 
 cd /tmp
 wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
 go version
 
 #Moving all above GO bins to /usr/bin
@@ -45,14 +45,16 @@ go get -u github.com/tomnomnom/anew
 #Install assetfinsder
 echo -e "${RED}[+] Installing assetfinder${NC}"
 go get -u github.com/tomnomnom/assetfinder
+mv ~/go/bin/assetfinder /usr/local/bin
 
 #Install assetfinder
 echo -e "${RED}[+] Installing subfinder${NC}"
 GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+mv ~/go/bin/subfinder /usr/local/bin
 
 #Install amass
 echo -e "${RED}[+] Installing amass${NC}"
-sudo snap install amass
+snap install amass
 
 #Install findomain
 echo -e "${RED}[+] Installing findomain${NC}"
@@ -61,7 +63,3 @@ wget https://github.com/findomain/findomain/releases/latest/download/findomain-l
 mv findomain-linux findomain
 mv findomain /usr/bin
 chmod +x /usr/bin/findomain
-
-#Moving all above GO bins to /usr/bin
-mv /usr/local/go/bin/* /usr/bin
-mv ~/go/bin/* /usr/bin
