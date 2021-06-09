3aa6386642d99804c44bd2692b280065922722d0
Tiago Tavares <0xtiago>
2021-06-09 13:55:06 -0300

    Primeiro commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Update repositories and install packages
+
+apt update 
+sudo apt upgrade -y
+sudo apt install python3-pip tmux -y
+
+#Install Golang
+cd /tmp
+wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
+
